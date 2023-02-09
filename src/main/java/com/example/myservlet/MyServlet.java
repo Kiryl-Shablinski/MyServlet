73e703dcbe9328cd944069038c1be6025ebf6948
Кирилл Шаблинский <59066332+Kiryl-Shablinski@users.noreply.github.com>
2023-02-09 17:32:18 +0300

    version 1.0

---
+++ src/main/java/com/example/myservlet/MyServlet.java
@@ -0,0 +1,19 @@
+package com.example.myservlet;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "MyServlet", value = "/MyServlet")
+public class MyServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter pw = response.getWriter();
+        pw.println("<html>");
+        pw.println("<h2> It's my first servlet</h2>");
+        pw.println("</html>");
+    }
+}
