90d3ee61ab98121e0ba1c6928a5aca60d08d133e
Кирилл Шаблинский <59066332+Kiryl-Shablinski@users.noreply.github.com>
2023-02-11 13:32:57 +0300

    version 1.1

--- src/main/java/com/example/myservlet/MyServlet.java
+++ src/main/java/com/example/myservlet/MyServlet.java
@@ -5,6 +5,8 @@ import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.stream.Stream;
+
 
 @WebServlet(name = "MyServlet", value = "/MyServlet")
 public class MyServlet extends HttpServlet {
@@ -12,8 +14,14 @@ public class MyServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
         PrintWriter pw = response.getWriter();
+        String number = request.getParameter("number");
+       String result = increase(number);
         pw.println("<html>");
-        pw.println("<h2> It's my first servlet</h2>");
+        pw.println("<h2>Increasing number: " + number + "<br>Result: "+ result + "</h2>");
         pw.println("</html>");
+    }
+
+    private String increase(String number){
+        return number !=null ? String.valueOf(Integer.parseInt(number) * 2) : null;
     }
 }
