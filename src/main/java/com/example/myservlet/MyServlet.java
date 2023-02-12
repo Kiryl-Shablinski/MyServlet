bbd06cce14a76166c05e993a80cbc7abbc63c694
Кирилл Шаблинский <59066332+Kiryl-Shablinski@users.noreply.github.com>
2023-02-12 14:41:47 +0300

    version 1.3 перенапрвление запроса

--- src/main/java/com/example/myservlet/MyServlet.java
+++ src/main/java/com/example/myservlet/MyServlet.java
@@ -7,33 +7,21 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 
-
 @WebServlet(name = "MyServlet", value = "/MyServlet")
 public class MyServlet extends HttpServlet {
+
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-       String x = req.getParameter("x");
-        String y = req.getParameter("y");
-        String operation = req.getParameter("operation");
-
-        String result =  operation(x, y, operation);
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String id = req.getParameter("id");
+        resp.setContentType("text/html");
         PrintWriter pw = resp.getWriter();
-        pw.println("<html><h3> Result:</h3><h2>" + result + "</h2></html>");
-    }
-
-        private String operation(String x, String y, String action){
-            int a = Integer.parseInt(x);
-            int b = Integer.parseInt(y);
-            switch (action){
-                case "+":
-                    return String.valueOf(a + b);
-                case "-":
-                    return String.valueOf(a - b);
-                case "*":
-                    return String.valueOf(a * b);
-                case "/":
-                    return String.format("%.2f",(double) a / b);
-            }
-            return null;
+        if (id != null){
+                pw.println("<html> <h2>Id: " + id + "</h2></html>");
+        }else {
+            String path = "/NotFoundServlet";
+            ServletContext context = getServletContext();
+            RequestDispatcher dispatcher = context.getRequestDispatcher(path);
+            dispatcher.forward(req, resp);
         }
+    }
 }
---
+++ src/main/java/com/example/myservlet/NotFoundServlet.java
@@ -0,0 +1,17 @@
+package com.example.myservlet;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "NotFoundServlet", value = "/NotFoundServlet")
+public class NotFoundServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter pw = response.getWriter();
+        pw.println("<html><h3>id not found</h3></html>");
+    }
+}
