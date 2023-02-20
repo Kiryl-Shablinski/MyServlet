c0571948ec64e4b192467db86b62cdbd7155e4b8
Кирилл Шаблинский <59066332+Kiryl-Shablinski@users.noreply.github.com>
2023-02-20 21:48:32 +0300

    version 2.0 JSTL

---
+++ src/main/java/com/example/myservlet/ServletJstl.java
@@ -0,0 +1,23 @@
+package com.example.myservlet;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+@WebServlet(name = "ServletJstl", value = "/jstl")
+public class ServletJstl extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+     getServletContext().getRequestDispatcher("/jstl.jsp").forward(request,response);
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
---
+++ src/main/webapp/jstl.jsp
@@ -0,0 +1,21 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: kirya
+  Date: 15.02.2023
+  Time: 21:23
+  To change this template use File | Settings | File Templates.
+--%>
+
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+
+<html>
+<head>
+    <title>JSTL in JSP</title>
+</head>
+<body>
+<c:if test="${34 > 467}">
+  Условие истинно
+</c:if>
+<p>JSTl worked</p>
+</body>
+</html>
